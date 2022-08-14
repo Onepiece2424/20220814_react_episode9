15a704b895ade596113df0e18ef44062dca0407e
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-08-14 10:14:48 +0900

    カスタムフックの作成と不要な部分の削除

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,11 @@
 import { useState } from "react";
-import axios from "axios";
+import { useFetchUsers } from "./hooks/useFetchUsers";
 
 export const App = () => {
-  const [userList, setUserList] = useState([]);
+  const { userList, onClickFetchUser } = useFetchUsers();
+  console.log(userList); // [{ id: 1 }]
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
-
-  //　ユーザー取得ボタン押下アクション
-  const onClickFetchUser = () => {
-    // ボタン押下時にローディングフラグon、エラーフラグoff
-    setIsLoading(true);
-    setIsError(false);
-
-    // APIの実行
-    axios
-      .get("https://example.com/users")
-      .then((result) => {
-        // 苗字と名前を結合するように変換
-        const users = result.data.map((user) => ({
-          id: user.id,
-          name: `${user.lastname} ${user.firstname}`,
-          age: user.age
-        }));
-        // ユーザー一覧Stateを更新
-        setUserList(users);
-      })
-      // エラーの場合はエラーフラグをon
-      .catch(() => setIsError(true))
-      // 処理完了後はローディングフラグをoff
-      .finally(() => setIsLoading(false));
-  };
 
   return (
     <div>
--- src/hooks/useFetchUsers.js
+++ src/hooks/useFetchUsers.js
@@ -1,1 +1,9 @@
-export const useFetchUsers = () => {};
+import { useState } from "react";
+export const useFetchUsers = () => {
+  const [userList, setUserList] = useState([{ id: 1 }]);
+
+  const onClickFetchUser = () => alert("関数実行");
+
+  // まとめて返却したいので、オブジェクトに設定する
+  return { userList, onClickFetchUser };
+};
